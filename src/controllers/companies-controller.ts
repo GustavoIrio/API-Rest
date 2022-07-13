26deaaf7feb62ea41389bc77594ddbca3786f84c
Gustavo Irio Buss <gustavoirio@hotmail.com>
2022-07-12 23:14:22 -0300

    Companies Endpoint

--- src/controllers/companies-controller.ts
+++ src/controllers/companies-controller.ts
@@ -11,5 +11,73 @@ export const companiesController = {
                 return res.status(400).json({ message: err.message })
             }
         }
-    }
+    },
+
+    // POST /companies
+    save: async (req: Request, res: Response) => {
+        const {  name, bio, website, email } = req.body
+        try {
+            const companies = await Company.create({
+                name,
+                bio,
+                website,
+                email
+            })
+            return res.status(201).json(companies)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // GET /companies/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const companies = await Company.findByPk(id)
+            return res.status(201).json(companies)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // PUT /companies/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params
+        const {  name, bio, website, email } = req.body
+        try {
+            const [affectedRows, companies] = await Company.update({
+                name,
+                bio,
+                website,
+                email
+            }, {
+                where: { id },
+                returning: true // retorna as linhas afetadas
+            })
+
+            return res.json(companies[0])
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // DELETE /companies/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const companies = await Company.destroy({ where: { id }})
+            return res.status(204).send()
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
 }
